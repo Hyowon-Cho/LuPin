9427aa1c8f599e11c51cd5623758e367b9a05ccd
Hyowon Cho <ardese@naver.com>
2025-08-31 21:33:28 -0400

    Update urls.py

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -1,32 +1,12 @@
-"""pragmatic URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from articleapp.views import ArticleListView
-# 추가
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    # 기존: path('', ArticleListView.as_view(), name='home'),
-    path('', TemplateView.as_view(template_name='temp.html'), name='home'),
-
-    # 아래 기존 라우트들은 그대로 두세요
+    path('', ArticleListView.as_view(), name='home'),  # ✅ 원래 홈으로 복구
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
