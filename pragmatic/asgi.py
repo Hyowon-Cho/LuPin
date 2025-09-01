08cc2a988d2e5d437ab5309b8086e6a3952fed44
Hyowon Cho <ardese@naver.com>
2025-08-31 21:05:35 -0400

    Update asgi.py

--- pragmatic/asgi.py
+++ pragmatic/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pragmatic.settings')
+# pragmatic/asgi.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pragmatic.settings.base')
 
 application = get_asgi_application()
